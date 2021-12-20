001ec332db1d0a9aa949799ab50259a67e0a28e9
Andrei Potapov <andr3008@gmail.com>
2021-12-20 19:56:46 +0200

    add form input

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { Component } from 'react';
 import './App.css';
+import ContactForm from './ContactForm/ContactForm';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  state = {
+    contacts: [],
+  };
+  formSubmitHandler = data => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.formSubmitHandler} />
+
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
 }
 
 export default App;
